a584bb15835c34e256acccf70269d1d124b56ffb
Rakesh K <rakeshforareason1@gmail.com>
2025-09-17 14:45:12 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '~l

--- src/components/map/map-view.tsx
+++ src/components/map/map-view.tsx
@@ -2,7 +2,7 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import 'leaflet-defaulticon-compatibility';
 
 import { mockDonations } from '@/lib/data';
