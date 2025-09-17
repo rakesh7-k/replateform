d151e19110bbd851ccb095e154e0208418e8ae57
Rakesh K <rakeshforareason1@gmail.com>
2025-09-17 14:50:48 +0000

    Try fixing this error: `Runtime Error: Error: window is not defined. Err

--- src/app/(app)/map/page.tsx
+++ src/app/(app)/map/page.tsx
@@ -1,9 +1,19 @@
-import DynamicMap from '@/components/map/dynamic-map';
+import dynamic from 'next/dynamic';
+import { useMemo } from 'react';
 
 export default function MapPage() {
+  const MapView = useMemo(
+    () =>
+      dynamic(() => import('@/components/map/map-view'), {
+        loading: () => <p>A map is loading...</p>,
+        ssr: false,
+      }),
+    []
+  );
+
   return (
     <div className="h-[calc(100vh-4rem)]">
-        <DynamicMap />
+      <MapView />
     </div>
   );
 }
